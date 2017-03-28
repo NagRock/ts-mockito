a5396b30dffb31a4bb1ad968ccea55a1ce1b27f6
NagRock <kuster.maciej@gmail.com>
2017-03-28 16:18:16 +0200

    Updated TypeScript and PhantomJS dependencies

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['PhantomJS2'],
+        browsers: ['PhantomJS'],
         singleRun: false
     };
 
