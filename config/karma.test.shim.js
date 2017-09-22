1b32087c61f53bd89c73a3a5b8573a31d821165c
NagRock <kuster.maciej@gmail.com>
2017-09-22 11:12:21 +0200

    maintenance/jest - removed karma and webpack config files, and jasmine types from tsconfig files

--- config/karma.test.shim.js
+++
@@ -1,8 +0,0 @@
-Error.stackTraceLimit = Infinity;
-
-require('core-js/es6');
-
-require('ts-helpers');
-
-var appContext = require.context('./../test', true, /^((?!e2e).)*spec/);
-appContext.keys().forEach(appContext);
