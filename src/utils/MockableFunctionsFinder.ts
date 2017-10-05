12cdc93772ec5140bb8e473c00fddc2bc8d0a3d8
Łukasz Bendykowski <lukasz.bendykowski@gmail.com>
2017-10-05 21:34:45 +0200

    Refactor MockableFunctionsFinder

--- src/utils/MockableFunctionsFinder.ts
+++ src/utils/MockableFunctionsFinder.ts
@@ -3,19 +3,25 @@ export class MockableFunctionsFinder {
 
     public find(code: string): {} {
         const result: any = {};
-        const subCodes: Array<string> = code.match(/\.(.{0,40})\(/g);
-        if (subCodes && subCodes.length > 0) {
-            for (const subCode of subCodes) {
-                const methodNames = subCode.match(/[a-zA-Z_][a-zA-Z0-9_$]+/g);
-                if (methodNames && methodNames.length > 0) {
-                    for (const methodName of methodNames) {
-                        if (this.notMockableFunctionNames.indexOf(methodName) < 0) {
-                            result[methodName] = true;
-                        }
-                    }
+        this.getSubCodes(code).forEach((subCode: string) => {
+            this.getMethodNames(subCode).forEach((methodName: string) => {
+                if (this.isMockable(methodName)) {
+                    result[methodName] = true;
                 }
-            }
-        }
+            });
+        });
         return result;
+    }
+
+    public isMockable(name: string): boolean {
+        return this.notMockableFunctionNames.indexOf(name) < 0;
+    }
+
+    private getSubCodes(code: string): string[] {
+        return code.match(/\.(.{0,40})\(/g) || [];
+    }
+
+    private getMethodNames(subCode: string): string[] {
+        return subCode.match(/[a-zA-Z_][a-zA-Z0-9_$]+/g) || [];
     }
 }
