0b18158cc958a58b1dcb3f60ac3064669a71fc0b
Steven Rosato <steven.rosato@majisti.com>
2020-08-18 17:08:20 -0400

    objectContaining matcher to output object representation when a test is failing

--- test/matcher/type/ObjectContainingMatcher.spec.ts
+++ test/matcher/type/ObjectContainingMatcher.spec.ts
@@ -24,12 +24,49 @@ describe("ObjectContainingMatcher", () => {
         });
 
         describe("when given value doesn't contain given object", () => {
-            it("returns false", () => {
-                // when
-                const result = testObj.match({b: {c: "c"}});
+            describe("when given value is an object", () => {
+                it("returns false", () => {
+                    // when
+                    const result = testObj.match({b: {c: "c"}});
 
-                // then
-                expect(result).toBeFalsy();
+                    // then
+                    expect(result).toBeFalsy();
+                });
+
+                it("represents the object as a json string", () => {
+                    // when
+                    const result = testObj.toString();
+
+                    // then
+                    expect(result).toContain(`objectContaining({"b":{"c":"c","d":{}}`);
+                });
+            });
+
+            describe("when given value is a scalar", () => {
+                const testCases = [
+                    { scalar: 123, expected: 123 },
+                    { scalar: 123.456, expected: 123.456 },
+                    { scalar: "a", expected: '"a"' },
+                ];
+
+                testCases.forEach(test => {
+                    it(`represents the scalar value '${test.scalar}' as string`, () => {
+                        // when
+                        const testValue: Matcher = objectContaining(test.scalar);
+                        const result = testValue.toString();
+
+                        // then
+                        expect(result).toContain(`objectContaining(${test.expected})`);
+                    });
+
+                    it("returns false", () => {
+                        // when
+                        const result = testObj.match(test.scalar);
+
+                        // then
+                        expect(result).toBeFalsy();
+                    });
+                });
             });
         });
     });
