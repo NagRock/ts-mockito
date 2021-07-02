a1be0e5fb664990bcad51e960a216609a424b0dd
Mike Porter <m_porter@live.co.uk>
2021-07-03 00:01:57 +0100

    Added better verification message feature from @johanblumenbergs fork

--- src/utils/MethodCallToStringConverter.ts
+++ src/utils/MethodCallToStringConverter.ts
@@ -1,9 +1,14 @@
 import {Matcher} from "../matcher/type/Matcher";
+import {MethodAction} from "../MethodAction";
 import {MethodToStub} from "../MethodToStub";
 
 export class MethodCallToStringConverter {
     public convert(method: MethodToStub): string {
         const stringifiedMatchers = method.matchers.map((matcher: Matcher) => matcher.toString()).join(", ");
         return `${method.name}(${stringifiedMatchers})" `;
+    }
+
+    public convertActualCalls(calls: MethodAction[]): string[] {
+        return calls.map(call => `${call.methodName}(${call.args.map(arg => arg.toString()).join(", ")})`);
     }
 }
